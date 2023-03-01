5db98b32c233c55aa5e82a505658719e6d7cda63
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-03-01 04:57:53 +0300

    Added steps of test

---
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -0,0 +1,24 @@
+package github;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideRepositorySearch {
+
+    @Test
+    void ShouldFindSekenideRepositoryAtTheTop() {
+
+        // открыть главную
+        open("https://github.com");
+        // ввести в поле поиска selenide и нажать enter
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        // кликнуть на первый репозиторий из списка найденных
+        $$("ul.repo-list li").first().$("a").click();
+        // проверка: заголовок selenide/selenide
+        sleep(5000);
+    }
+
+}
+
