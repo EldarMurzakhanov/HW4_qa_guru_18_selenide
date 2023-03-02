822cbdd0526b412860f418623c611cb73dda2814
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-03-03 02:08:06 +0300

    Added new class

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,8 +1,10 @@
 package github;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideRepositorySearch {
@@ -17,6 +19,7 @@ public class SelenideRepositorySearch {
         // кликнуть на первый репозиторий из списка найденных
         $$("ul.repo-list li").first().$("a").click();
         // проверка: заголовок selenide/selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
         sleep(5000);
     }
 
